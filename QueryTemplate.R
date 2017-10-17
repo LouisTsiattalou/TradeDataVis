3c0928cd5d289f31201d72b89a92865100da76d4
Louis Tsiattalou <louis.tsi@gmail.com>
2017-10-17 16:37:11 +0100

    change file convention, add port and country metadata table

--- QueryTemplate.R
+++ QueryTemplate.R
@@ -13,6 +13,10 @@ library("RPostgreSQL")
 if(require("XLConnect") == FALSE) {install.packages("XLConnect")}
 library("XLConnect")
 
+setwd("C:/Users/ltsiattalou/Documents/R/ImportTool/")
+suppressWarnings(dir.create(paste(getwd(), "/dbextract", sep = "")))
+setwd("dbextract")
+
 # DEFINE CONNECTION ----------------------------------------------------------
 
 pg = dbDriver("PostgreSQL")
@@ -21,6 +25,16 @@ tradedata = dbConnect(pg, user="postgres", password="postgres",
 
 # DEFINE QUERY AND EXECUTE ---------------------------------------------------
 
+# sqlquery <- "SELECT exports.comcode,
+#                     comcode.description,
+#                     exports.cod_alpha,
+#                     exports.account_date,
+#                     exports.port_alpha,
+#                     exports.value,
+#                     exports.quantity_1
+#               FROM exports
+#               LEFT JOIN comcode ON exports.comcode = comcode.commoditycode
+#               WHERE account_date LIKE '%2016%' AND comcode LIKE '03______';"
 sqlquery <- "SELECT exports.comcode,
                     comcode.description,
                     exports.cod_alpha,
@@ -30,7 +44,8 @@ sqlquery <- "SELECT exports.comcode,
                     exports.quantity_1
               FROM exports
               LEFT JOIN comcode ON exports.comcode = comcode.commoditycode
-              WHERE account_date LIKE '%2016%' AND comcode LIKE '03______';"
+              WHERE account_date LIKE '%2016%' AND comcode LIKE '0301____';"
+
 
 data <- dbGetQuery(tradedata,sqlquery)
 
@@ -43,4 +58,3 @@ wkb <- loadWorkbook("extract.xlsx", create = TRUE)
 createSheet(wkb, "PostgreSQL Extract")
 writeWorksheet(wkb, data, sheet = "PostgreSQL Extract")
 saveWorkbook(wkb)
-closeWorkbook(wkb)
